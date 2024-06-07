b6282b6418931386c687a365dfb355be2f6ef541
VNNICE <165227660+VNNICE@users.noreply.github.com>
2024-06-07 17:57:15 +0900

    20240607

--- WHMS/Add_ItemList.Designer.cs
+++ WHMS/Add_ItemList.Designer.cs
@@ -31,13 +31,13 @@
             components = new System.ComponentModel.Container();
             contextMenuStrip1 = new ContextMenuStrip(components);
             button_Decide = new Button();
-            text_Object = new Label();
-            TextBox_Type = new Label();
-            text_AssetType = new Label();
-            text_Name = new Label();
-            text_Manufacturer = new Label();
-            text_SerialNumber = new Label();
-            text_Quantity = new Label();
+            label_Object = new Label();
+            label_Type = new Label();
+            label_AssetType = new Label();
+            label_Name = new Label();
+            label_Manufacturer = new Label();
+            label_SerialNumber = new Label();
+            label_Quantity = new Label();
             text_Memo = new Label();
             textBox_Name = new TextBox();
             textBox_SerialNumber = new TextBox();
@@ -45,11 +45,11 @@
             comboBox_Object = new ComboBox();
             button_Cancel = new Button();
             textBox_Price = new TextBox();
-            label1 = new Label();
+            label_Price = new Label();
             comboBox_Type = new ComboBox();
             comboBox_AssetType = new ComboBox();
             comboBox_Manufacturer = new ComboBox();
-            richTextBox1 = new RichTextBox();
+            richTextBox_Memo = new RichTextBox();
             button_DisplayMemo = new Button();
             SuspendLayout();
             // 
@@ -63,72 +63,72 @@
             button_Decide.Location = new Point(10, 274);
             button_Decide.Name = "button_Decide";
             button_Decide.Size = new Size(75, 23);
-            button_Decide.TabIndex = 4;
+            button_Decide.TabIndex = 9;
             button_Decide.Text = "確定";
             button_Decide.UseVisualStyleBackColor = true;
             // 
-            // text_Object
+            // label_Object
             // 
-            text_Object.AutoSize = true;
-            text_Object.Location = new Point(10, 12);
-            text_Object.Name = "text_Object";
-            text_Object.Size = new Size(55, 15);
-            text_Object.TabIndex = 5;
-            text_Object.Text = "使用目的";
+            label_Object.AutoSize = true;
+            label_Object.Location = new Point(10, 12);
+            label_Object.Name = "label_Object";
+            label_Object.Size = new Size(55, 15);
+            label_Object.TabIndex = 5;
+            label_Object.Text = "使用目的";
             // 
-            // TextBox_Type
+            // label_Type
             // 
-            TextBox_Type.AutoSize = true;
-            TextBox_Type.Location = new Point(27, 42);
-            TextBox_Type.Name = "TextBox_Type";
-            TextBox_Type.Size = new Size(31, 15);
-            TextBox_Type.TabIndex = 6;
-            TextBox_Type.Text = "種類";
+            label_Type.AutoSize = true;
+            label_Type.Location = new Point(27, 42);
+            label_Type.Name = "label_Type";
+            label_Type.Size = new Size(31, 15);
+            label_Type.TabIndex = 6;
+            label_Type.Text = "種類";
             // 
-            // text_AssetType
+            // label_AssetType
             // 
-            text_AssetType.AutoSize = true;
-            text_AssetType.Location = new Point(12, 71);
-            text_AssetType.Name = "text_AssetType";
-            text_AssetType.Size = new Size(55, 15);
-            text_AssetType.TabIndex = 7;
-            text_AssetType.Text = "資産管理";
+            label_AssetType.AutoSize = true;
+            label_AssetType.Location = new Point(12, 71);
+            label_AssetType.Name = "label_AssetType";
+            label_AssetType.Size = new Size(55, 15);
+            label_AssetType.TabIndex = 7;
+            label_AssetType.Text = "資産管理";
             // 
-            // text_Name
+            // label_Name
             // 
-            text_Name.AutoSize = true;
-            text_Name.Location = new Point(27, 100);
-            text_Name.Name = "text_Name";
-            text_Name.Size = new Size(31, 15);
-            text_Name.TabIndex = 8;
-            text_Name.Text = "品名";
+            label_Name.AutoSize = true;
+            label_Name.Location = new Point(27, 100);
+            label_Name.Name = "label_Name";
+            label_Name.Size = new Size(31, 15);
+            label_Name.TabIndex = 8;
+            label_Name.Text = "品名";
             // 
-            // text_Manufacturer
+            // label_Manufacturer
             // 
-            text_Manufacturer.AutoSize = true;
-            text_Manufacturer.Location = new Point(25, 129);
-            text_Manufacturer.Name = "text_Manufacturer";
-            text_Manufacturer.Size = new Size(40, 15);
-            text_Manufacturer.TabIndex = 9;
-            text_Manufacturer.Text = "メーカー";
+            label_Manufacturer.AutoSize = true;
+            label_Manufacturer.Location = new Point(25, 129);
+            label_Manufacturer.Name = "label_Manufacturer";
+            label_Manufacturer.Size = new Size(40, 15);
+            label_Manufacturer.TabIndex = 9;
+            label_Manufacturer.Text = "メーカー";
             // 
-            // text_SerialNumber
+            // label_SerialNumber
             // 
-            text_SerialNumber.AutoSize = true;
-            text_SerialNumber.Location = new Point(27, 158);
-            text_SerialNumber.Name = "text_SerialNumber";
-            text_SerialNumber.Size = new Size(31, 15);
-            text_SerialNumber.TabIndex = 10;
-            text_SerialNumber.Text = "型番";
+            label_SerialNumber.AutoSize = true;
+            label_SerialNumber.Location = new Point(27, 158);
+            label_SerialNumber.Name = "label_SerialNumber";
+            label_SerialNumber.Size = new Size(31, 15);
+            label_SerialNumber.TabIndex = 10;
+            label_SerialNumber.Text = "型番";
             // 
-            // text_Quantity
+            // label_Quantity
             // 
-            text_Quantity.AutoSize = true;
-            text_Quantity.Location = new Point(27, 216);
-            text_Quantity.Name = "text_Quantity";
-            text_Quantity.Size = new Size(31, 15);
-            text_Quantity.TabIndex = 11;
-            text_Quantity.Text = "数量";
+            label_Quantity.AutoSize = true;
+            label_Quantity.Location = new Point(27, 216);
+            label_Quantity.Name = "label_Quantity";
+            label_Quantity.Size = new Size(31, 15);
+            label_Quantity.TabIndex = 11;
+            label_Quantity.Text = "数量";
             // 
             // text_Memo
             // 
@@ -144,21 +144,21 @@
             textBox_Name.Location = new Point(82, 97);
             textBox_Name.Name = "textBox_Name";
             textBox_Name.Size = new Size(100, 23);
-            textBox_Name.TabIndex = 13;
+            textBox_Name.TabIndex = 3;
             // 
             // textBox_SerialNumber
             // 
             textBox_SerialNumber.Location = new Point(83, 155);
             textBox_SerialNumber.Name = "textBox_SerialNumber";
             textBox_SerialNumber.Size = new Size(100, 23);
-            textBox_SerialNumber.TabIndex = 15;
+            textBox_SerialNumber.TabIndex = 5;
             // 
             // textBox_Quantity
             // 
             textBox_Quantity.Location = new Point(81, 212);
             textBox_Quantity.Name = "textBox_Quantity";
             textBox_Quantity.Size = new Size(100, 23);
-            textBox_Quantity.TabIndex = 16;
+            textBox_Quantity.TabIndex = 7;
             // 
             // comboBox_Object
             // 
@@ -168,14 +168,14 @@
             comboBox_Object.Location = new Point(82, 9);
             comboBox_Object.Name = "comboBox_Object";
             comboBox_Object.Size = new Size(99, 23);
-            comboBox_Object.TabIndex = 18;
+            comboBox_Object.TabIndex = 0;
             // 
             // button_Cancel
             // 
             button_Cancel.Location = new Point(91, 274);
             button_Cancel.Name = "button_Cancel";
             button_Cancel.Size = new Size(90, 23);
-            button_Cancel.TabIndex = 19;
+            button_Cancel.TabIndex = 10;
             button_Cancel.Text = "キャンセル";
             button_Cancel.UseVisualStyleBackColor = true;
             // 
@@ -184,16 +184,16 @@
             textBox_Price.Location = new Point(83, 184);
             textBox_Price.Name = "textBox_Price";
             textBox_Price.Size = new Size(97, 23);
-            textBox_Price.TabIndex = 20;
+            textBox_Price.TabIndex = 6;
             // 
-            // label1
+            // label_Price
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(27, 187);
-            label1.Name = "label1";
-            label1.Size = new Size(31, 15);
-            label1.TabIndex = 21;
-            label1.Text = "価格";
+            label_Price.AutoSize = true;
+            label_Price.Location = new Point(27, 187);
+            label_Price.Name = "label_Price";
+            label_Price.Size = new Size(31, 15);
+            label_Price.TabIndex = 21;
+            label_Price.Text = "価格";
             // 
             // comboBox_Type
             // 
@@ -201,7 +201,7 @@
             comboBox_Type.Location = new Point(82, 39);
             comboBox_Type.Name = "comboBox_Type";
             comboBox_Type.Size = new Size(99, 23);
-            comboBox_Type.TabIndex = 22;
+            comboBox_Type.TabIndex = 1;
             // 
             // comboBox_AssetType
             // 
@@ -209,7 +209,7 @@
             comboBox_AssetType.Location = new Point(82, 68);
             comboBox_AssetType.Name = "comboBox_AssetType";
             comboBox_AssetType.Size = new Size(99, 23);
-            comboBox_AssetType.TabIndex = 23;
+            comboBox_AssetType.TabIndex = 2;
             // 
             // comboBox_Manufacturer
             // 
@@ -217,22 +217,23 @@
             comboBox_Manufacturer.Location = new Point(82, 126);
             comboBox_Manufacturer.Name = "comboBox_Manufacturer";
             comboBox_Manufacturer.Size = new Size(101, 23);
-            comboBox_Manufacturer.TabIndex = 24;
+            comboBox_Manufacturer.TabIndex = 4;
             // 
-            // richTextBox1
+            // richTextBox_Memo
             // 
-            richTextBox1.Location = new Point(191, 88);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(225, 209);
-            richTextBox1.TabIndex = 25;
-            richTextBox1.Text = "";
+            richTextBox_Memo.Enabled = false;
+            richTextBox_Memo.Location = new Point(191, 88);
+            richTextBox_Memo.Name = "richTextBox_Memo";
+            richTextBox_Memo.Size = new Size(225, 209);
+            richTextBox_Memo.TabIndex = 25;
+            richTextBox_Memo.Text = "";
             // 
             // button_DisplayMemo
             // 
             button_DisplayMemo.Location = new Point(12, 241);
             button_DisplayMemo.Name = "button_DisplayMemo";
             button_DisplayMemo.Size = new Size(173, 27);
-            button_DisplayMemo.TabIndex = 26;
+            button_DisplayMemo.TabIndex = 8;
             button_DisplayMemo.Text = "備考登録 >>";
             button_DisplayMemo.UseVisualStyleBackColor = true;
             // 
@@ -242,11 +243,11 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(432, 309);
             Controls.Add(button_DisplayMemo);
-            Controls.Add(richTextBox1);
+            Controls.Add(richTextBox_Memo);
             Controls.Add(comboBox_Manufacturer);
             Controls.Add(comboBox_AssetType);
             Controls.Add(comboBox_Type);
-            Controls.Add(label1);
+            Controls.Add(label_Price);
             Controls.Add(textBox_Price);
             Controls.Add(button_Cancel);
             Controls.Add(comboBox_Object);
@@ -254,13 +255,13 @@
             Controls.Add(textBox_SerialNumber);
             Controls.Add(textBox_Name);
             Controls.Add(text_Memo);
-            Controls.Add(text_Quantity);
-            Controls.Add(text_SerialNumber);
-            Controls.Add(text_Manufacturer);
-            Controls.Add(text_Name);
-            Controls.Add(text_AssetType);
-            Controls.Add(TextBox_Type);
-            Controls.Add(text_Object);
+            Controls.Add(label_Quantity);
+            Controls.Add(label_SerialNumber);
+            Controls.Add(label_Manufacturer);
+            Controls.Add(label_Name);
+            Controls.Add(label_AssetType);
+            Controls.Add(label_Type);
+            Controls.Add(label_Object);
             Controls.Add(button_Decide);
             Name = "Add_ItemList";
             ResumeLayout(false);
@@ -273,13 +274,13 @@
         private TextBox input_AssetType;
         private TextBox input;
         private Button button_Decide;
-        private Label text_Object;
-        private Label TextBox_Type;
-        private Label text_AssetType;
-        private Label text_Name;
-        private Label text_Manufacturer;
-        private Label text_SerialNumber;
-        private Label text_Quantity;
+        private Label label_Object;
+        private Label label_Type;
+        private Label label_AssetType;
+        private Label label_Name;
+        private Label label_Manufacturer;
+        private Label label_SerialNumber;
+        private Label label_Quantity;
         private Label text_Memo;
         private TextBox textBox_Name;
         private TextBox input_Manufacturer;
@@ -288,11 +289,11 @@
         private ComboBox comboBox_Object;
         private Button button_Cancel;
         private TextBox textBox_Price;
-        private Label label1;
+        private Label label_Price;
         private ComboBox comboBox_Type;
         private ComboBox comboBox_AssetType;
         private ComboBox comboBox_Manufacturer;
-        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox_Memo;
         private Button button_DisplayMemo;
     }
 }
--- WHMS/Add_ItemList.cs
+++ WHMS/Add_ItemList.cs
@@ -28,12 +28,19 @@ namespace WHMS
         private int price;
         private int quantity;
         private string? memo;
+        private string errmsg;
 
         public Add_ItemList()
         {
             InitializeComponent();
             this.Size = new Size(204, 348);
             button_DisplayMemo.Click += (sender, e) => DisplayMemo();
+            button_Decide.Click += (sender, e) => AddData();
+        }
+        private void AddData()
+        {
+            SerializeText();
+            //ItemList itemList = new ItemList();
         }
 
         private void DisplayMemo() 
@@ -44,27 +51,77 @@ namespace WHMS
                 onClickMemo = true;
                 button_DisplayMemo.Text = "＜＜備考登録";
                 this.Size = new Size(440, 348);
-                
+                richTextBox_Memo.Enabled = true;
             }
             else
             {
                 onClickMemo = false;
                 button_DisplayMemo.Text = "備考登録＞＞";
                 this.Size = new Size(204, 348);
+                richTextBox_Memo.Enabled = false;
             }
         }
 
         private void SerializeText()
         {
-            obj = comboBox_Object.Text.ToString();
-            type = comboBox_Type.Text.ToString();
-            assetType = comboBox_AssetType.Text.ToString();
-            name = textBox_Name.Text.ToString();
-            manufacturer = comboBox_Manufacturer.Text.ToString();
-            serialNumber = textBox_SerialNumber.Text.ToString();
-            price = int.Parse(textBox_Price.Text);
-            quantity = int.Parse(textBox_Quantity.Text);
-            memo = text_Memo.Text.ToString();
+            try
+            {
+                obj = comboBox_Object.Text.ToString();
+                type = comboBox_Type.Text.ToString();
+                assetType = comboBox_AssetType.Text.ToString();
+                name = textBox_Name.Text.ToString();
+                manufacturer = comboBox_Manufacturer.Text.ToString();
+                serialNumber = textBox_SerialNumber.Text.ToString();
+                price = int.Parse(textBox_Price.Text);
+                quantity = int.Parse(textBox_Quantity.Text);
+                if (obj == "" || type == "" || assetType == "" || name == "" || manufacturer == "" || price == 0 || quantity == 0)
+                {
+                    throw new InvalidValues();
+                }
+                if (text_Memo.Text != null)
+                {
+                    memo = text_Memo.Text.ToString();
+                }
+            }
+            catch (InvalidValues)
+            {
+                errmsg = "次の入力値が無効です：\n";
+                if (obj == "")
+                {
+                    errmsg += $"　{{{label_Object.Text}}}\n";
+                }
+                if (type == "")
+                {
+                    errmsg += $"　{{{label_Type.Text}}}\n";
+                }
+                if (assetType == "")
+                {
+                    errmsg += $"　{{{label_Name.Text}}}\n";
+                }
+                if (name == "")
+                {
+                    errmsg += $"　{{{label_Manufacturer.Text}}}\n";
+                }
+                if (serialNumber == "")
+                {
+                    errmsg += $"　{{{label_SerialNumber.Text}}}\n";
+                }
+                if (price == 0)
+                {
+                    errmsg += $"　{{{label_Price.Text}}}\n";
+                }
+                if (quantity == 0)
+                {
+                    errmsg += $"　{{{label_Quantity.Text}}}\n";
+                }
+                MessageBox.Show(errmsg);
+                return;
+            }
+
         }
+    }
+    class InvalidValues() : Exception
+    { 
+    
     }
 }
